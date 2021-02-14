d6624a9b2b628020fa944650fb54b62554b2aa12
jwstegemann <jwstegemann@gmail.com>
2021-02-14 12:43:51 +0100

    first shot at pwa

--- src/jsMain/kotlin/app.kt
+++ src/jsMain/kotlin/app.kt
@@ -1,10 +1,38 @@
 import dev.fritz2.components.*
 import dev.fritz2.dom.html.render
 import dev.fritz2.styling.params.styled
+import kotlinx.browser.window
 import kotlinx.coroutines.flow.flowOf
+import org.w3c.workers.InstallEvent
+import org.w3c.workers.ServiceWorkerGlobalScope
+import org.w3c.workers.ServiceWorkerRegistration
+import kotlin.js.Promise
 
+external val self: ServiceWorkerGlobalScope
+
+const val CACHE_NAME = "my-site-cache-v1"
+val urlsToCache = arrayOf(
+"/",
+"/st􏰁les/main.css",
+"/images/dog.svg",
+"/images/cat.cvg"
+)
+fun installServiceWorker() { self.addEventListener("install", { event ->
+    event as InstallEvent
+    event.waitUntil(
+        self.caches.open(CACHE_NAME)
+            .then { it.addAll(urlsToCache) }
+    ) })
+}
 
 fun main() {
+    try {
+        window.addEventListener("load", {
+            window.navigator.serviceWorker.register("/serviceWorker.js") })
+            console.log("Service Worker registered")
+    } catch (t: Throwable) {
+            console.log("Error registering ervice Worker:", t)
+    }
 
     render {
         pwa {
@@ -49,11 +77,16 @@ fun main() {
 
             nav {
                 navSection("Section Header")
-                (1..5).forEach {
+                (1..5).forEach { number ->
+                    val text = "Link No. $number"
                     navLink {
                         icon { fromTheme { calendar } }
-                        text("Link No. $it")
-                        if (it == 3) active(flowOf(true))
+                        text(text)
+                        if (number == 3) active(flowOf(true))
+                    } handledBy alertToast {
+                        console.log("Number: $number")
+                        severity { info }
+                        content(text)
                     }
                 }
 
@@ -77,7 +110,7 @@ fun main() {
                         }) { fromTheme { user } }
                         (::span.styled {
                             fontSize { normal }
-                        }) { + "Jens" }
+                        }) { + "Jens Stegemann" }
                         pushButton {
                             variant { outline }
                             color { lightGray }
--- webpack.config.d/devServer.js
+++ webpack.config.d/devServer.js
@@ -1,3 +1,11 @@
 if (config.devServer) {
+//   config.devServer.host = "0.0.0.0"
+
    config.devServer.host = "0.0.0.0"
+   config.devServer.disableHostCheck = true
+   config.devServer.port = 8080
+   config.devServer.https = true
+   config.devServer.key = "/Users/tiberius/ca/herakles.local-key.pem"
+   config.devServer.cert = "/Users/tiberius/ca/herakles.local.pem"
+   config.devServer.ca = "/Users/tiberius/ca/herakles.local.pem"
 }
